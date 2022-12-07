3384d6eb18e4b489f2dcb5e1d3e00753a9966f90
Breno Alexandre <breno.alexandre@softdesign.com.br>
2022-12-06 23:24:06 -0300

    forwarding styles from a single index

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Header from './pages/Header';
-import './index.scss';
+import Header from './components/Header';
 import Sheet from './pages/Sheet';
+import './index.scss';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
